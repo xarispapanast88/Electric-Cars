97d1f99fc999cd5e578833948ca5df7448ef46ff
xarispapanast88 <67091209+xarispapanast88@users.noreply.github.com>
2020-06-18 09:39:54 +0300

    Add files via upload

---
+++ electric_cars/edit_station.php
@@ -0,0 +1,147 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecars";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM station";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+	
+echo "<table border = '1'>";
+
+echo "<tr>";
+
+echo "<th>";
+
+echo "Id";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Name";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Latitude";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Longitude";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Serives";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Places";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Rating";
+
+echo "</th>";
+
+echo "<th>";
+
+echo "Reserved";
+
+echo "</th>";
+
+echo "</tr>";
+
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+	echo "<td>";
+	
+	echo $row['id'];
+	
+   echo "</td>";
+	
+   echo "<td>";
+	
+	echo $row['name'];
+	
+   echo "</td>";
+   
+   echo "<td>";
+	
+	echo $row['lat'];
+	
+   echo "</td>";
+   
+   echo "<td>";
+	
+	echo $row['lng'];
+	
+   echo "</td>";
+   
+   echo "<td>";
+	
+	echo $row['services'];
+	
+   echo "</td>";
+   
+   echo "<td>";
+	
+	echo $row['places'];
+	
+   echo "</td>";
+	
+   
+   echo "<td>";
+	
+	echo $row['rating'];
+	
+   echo "</td>";
+   
+   echo "</td>";
+	
+   
+   echo "<td>";
+	
+	if ($row['reserved']=='1') echo "Yes";
+	
+	else echo "No";
+	
+   echo "</td>";
+   
+      echo "</tr>";
+
+  }
+  
+echo "</table>"; 
+  
+} else {
+  echo "0 results";
+}
+  
+  
+
+
+
+?>
